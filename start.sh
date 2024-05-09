9688188a9fdba2b3137e435afb81708e15205c4a
ltcevil <evil@evils.live>
2024-05-09 16:03:25 +0200

    .

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#supervisord -c /code/supervisord.conf
-# Start the Python application in the background
-uvicorn app.server:app --host 0.0.0.0 --port 8001 &
-
-# Start the Node.js application
-npm run start:server:prod
